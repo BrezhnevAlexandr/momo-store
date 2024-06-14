606c8fc59f6c458a560315c7c977be841c9aa04e
Alexandr Brezhnev <sharuman@mail.ru>
2024-06-14 20:43:14 +0000

    prod test 48

--- momo-store-chart/kubeconfig.sh
+++ momo-store-chart/kubeconfig.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# Вставляем ID и добавляем CLUSTER_ID в переменную окружения
+echo "1. Вставляем ID и добавляем CLUSTER_ID в переменную окружения"
 rm ~/.kube/config
 yc managed-kubernetes cluster get-credentials sharuman-k8s-cluster --external --force
+mkdir testconfig
+cd testconfig/
 
 export CLUSTER_ID=$(yc managed-kubernetes cluster list --format json | jq -r '.[] | select(.name=="sharuman-k8s-cluster") | .id')
 echo "CLUSTER_ID is $CLUSTER_ID"
 
-# Получаем сертификат
+echo "2. Получаем сертификат..."
 yc managed-kubernetes cluster get --id $CLUSTER_ID --format json | \
   jq -r .master.master_auth.cluster_ca_certificate | \
   awk '{gsub(/\\n/,"\n")}1' > ca.pem
 
 CERT_CONTENT=$(base64 -w 0 ca.pem)
 
-# Подготавливаем токен 
+echo "3. Подготавливаем токен" 
 SA_TOKEN=$(kubectl -n kube-system get secret $(kubectl -n kube-system get secret | \
   grep admin-user-token | \
   awk '{print $1}') -o json | \
   jq -r .data.token | \
   base64 -d)
   
-# Получаем IP-адрес кластера
+echo "4. Получаем IP-адрес кластера"
 MASTER_ENDPOINT=$(yc managed-kubernetes cluster get --id $CLUSTER_ID \
   --format json | \
   jq -r .master.endpoints.external_v4_endpoint)
   
-# Добавляем файл конфигурации:
+echo "5. Добавляем файл конфигурации"
 kubectl config set-cluster sharuman-k8s-cluster \
   --certificate-authority=/dev/null \
   --server=$MASTER_ENDPOINT \
@@ -34,12 +36,12 @@ kubectl config set-cluster sharuman-k8s-cluster \
 
 sed -i "s#certificate-authority: /dev/null#certificate-authority-data: ${CERT_CONTENT}#" test.kubeconfig
   
-# Добавляем токен
+echo "6. Добавляем токен к файлу"
 kubectl config set-credentials admin-user \
   --token=$SA_TOKEN \
   --kubeconfig=test.kubeconfig
   
-#Добавляем инфу о контексте:
+echo "7. Добавляем инфу о контексте"
 kubectl config set-context default \
   --cluster=sharuman-k8s-cluster \
   --user=admin-user \
@@ -48,6 +50,6 @@ kubectl config set-context default \
   
 kubectl config use-context default \
   --kubeconfig=test.kubeconfig
-  
+echo "8. Прибираемся за собой и меняем файл. Теперь доступен конфиг"  
 rm ~/.kube/config
 mv test.kubeconfig ~/.kube/config
