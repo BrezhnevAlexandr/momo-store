81286261c8c4afba1d5397b729de7d238ab16711
Alexandr Brezhnev <sharuman@mail.ru>
2024-06-15 01:43:04 +0000

    prod test 55

--- momo-store-chart/kubeconfig.sh
+++ momo-store-chart/kubeconfig.sh
@@ -1,62 +1,51 @@
 #!/bin/bash
 echo "0. Удаляем старый конфиг ~/.kube/config"
-rm ~/.kube/config
-echo "1. Вставляем ID и добавляем CLUSTER_ID в переменную окружения"
-echo "выполнение команды yc managed-kubernetes cluster get-credentials sharuman-k8s-cluster --external --force"
-yc managed-kubernetes cluster get-credentials sharuman-k8s-cluster --external --force
-echo "Результат выполнения предыдущей команды и факт подтверждения что yc установлен и работает"
-cat ~/.kube/config
- 
-mkdir testconfig
-cd testconfig/
+rm -f ~/.kube/config
 
+echo "1. Вставляем ID и добавляем CLUSTER_ID в переменную окружения"
 export CLUSTER_ID=$(yc managed-kubernetes cluster list --format json | jq -r '.[] | select(.name=="sharuman-k8s-cluster") | .id')
 echo "CLUSTER_ID is $CLUSTER_ID"
 
-echo "2. Получаем сертификат..."
-yc managed-kubernetes cluster get --id $CLUSTER_ID --format json | \
-  jq -r .master.master_auth.cluster_ca_certificate | \
-  awk '{gsub(/\\n/,"\n")}1' > ca.pem
+echo "2. Получаем учетные данные кластера"
+yc managed-kubernetes cluster get-credentials $CLUSTER_ID --external --force --kubeconfig=$KUBECONFIG_PATH
+if [ $? -ne 0 ]; then
+  echo "Ошибка получения учетных данных кластера"
+  exit 1
+fi
 
-CERT_CONTENT=$(base64 -w 0 ca.pem)
+echo "3. Проверка содержимого kubeconfig"
+cat $KUBECONFIG_PATH
 
-echo "3. Подготавливаем токен" 
-SA_TOKEN=$(kubectl -n kube-system get secret $(kubectl -n kube-system get secret | \
-  grep admin-user-token | \
-  awk '{print $1}') -o json | \
-  jq -r .data.token | \
-  base64 -d)
-  
-echo "4. Получаем IP-адрес кластера"
-MASTER_ENDPOINT=$(yc managed-kubernetes cluster get --id $CLUSTER_ID \
-  --format json | \
-  jq -r .master.endpoints.external_v4_endpoint)
-  
-echo "5. Добавляем файл конфигурации"
+echo "4. Получаем сертификат"
+CERT_CONTENT=$(yc managed-kubernetes cluster get --id $CLUSTER_ID --format json | jq -r .master.master_auth.cluster_ca_certificate | base64 -w 0)
+
+echo "5. Подготавливаем токен"
+SA_TOKEN=$(kubectl -n kube-system get secret $(kubectl -n kube-system get secret | grep admin-user-token | awk '{print $1}') -o json | jq -r .data.token | base64 -d)
+
+echo "6. Получаем IP-адрес кластера"
+MASTER_ENDPOINT=$(yc managed-kubernetes cluster get --id $CLUSTER_ID --format json | jq -r .master.endpoints.external_v4_endpoint)
+
+echo "7. Настройка kubeconfig"
 kubectl config set-cluster sharuman-k8s-cluster \
   --certificate-authority=/dev/null \
   --server=$MASTER_ENDPOINT \
   --kubeconfig=test.kubeconfig
 
 sed -i "s#certificate-authority: /dev/null#certificate-authority-data: ${CERT_CONTENT}#" test.kubeconfig
-  
-echo "6. Добавляем токен к файлу"
+
 kubectl config set-credentials admin-user \
   --token=$SA_TOKEN \
   --kubeconfig=test.kubeconfig
-  
-echo "7. Добавляем инфу о контексте"
+
 kubectl config set-context default \
   --cluster=sharuman-k8s-cluster \
   --user=admin-user \
   --kubeconfig=test.kubeconfig
-  
-  
-kubectl config use-context default \
-  --kubeconfig=test.kubeconfig
-echo "8. Прибираемся за собой и меняем файл. Теперь доступен новый конфиг:"
-rm ~/.kube/config
-mv test.kubeconfig ~/.kube/config
-cd ..
-rm -rf testconfig/
-cat  ~/.kube/config
+
+kubectl config use-context default --kubeconfig=test.kubeconfig
+
+echo "8. Перемещение нового конфигурационного файла"
+mv test.kubeconfig $KUBECONFIG_PATH
+
+echo "9. Проверка конечного содержимого kubeconfig"
+cat $KUBECONFIG_PATH
